83973b2e9462908b02c530d20b173d8c68b52720
Rick Mercer <rmercer33@gmail.com>
2020-06-09 15:17:07 -1000

    added language support to shared code

--- src/js/constants.js
+++ src/js/constants.js
@@ -6,7 +6,9 @@ const keyInfo = require("./modules/_config/key");
 import {getPageInfo} from "./modules/_config/config";
 
 export default {
-  sid: "ACIM",
+  sid: "acim",
+  env: "integration",                      //sa or prod, sa=standalone
+  lang: "en",
   getPageInfo: getPageInfo,              //list
   keyInfo: keyInfo,                      //list, bmnet
   bm_modal_key: "bm.acim.modal",         //list
