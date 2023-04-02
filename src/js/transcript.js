9cc6bd700810a43d8d08792618ecd8d1703195f0
Rick Mercer <rmercer33@gmail.com>
2023-04-01 16:14:44 -1000

    added support for saved searches

--- src/js/transcript.js
+++ src/js/transcript.js
@@ -1,5 +1,6 @@
 /* eslint no-console: off */
-import {storeInit} from "www/modules/_util/store";
+import {SourceStore, storeInit} from "www/modules/_util/store";
+import search from "www/modules/_search/search";
 
 //common modules
 import {showParagraph} from "www/modules/_util/url";
@@ -9,7 +10,7 @@ import {initTranscriptPage} from "www/modules/_page/startup";
 
 import {loadConfig} from "./modules/_config/config";
 import {bookmarkStart} from "./modules/_bookmark/start";
-import search from "./modules/_search/search";
+import { searchInit } from "./modules/_search/search";
 import toc, {getBookId} from "./modules/_contents/toc";
 import audio from "./modules/_audio/audio";
 import about from "./modules/_about/about";
@@ -18,16 +19,18 @@ import {setLanguage} from "www/modules/_language/lang";
 import constants from "./constants";
 
 $(document).ready(() => {
+  const store = new SourceStore(constants);
   storeInit(constants);
   setLanguage(constants);
-  initTranscriptPage("pnDisplay");
+
   auth.initialize();
+  initTranscriptPage("pnDisplay");
   fb.initialize();
   about.initialize();
 
   //load config file and do initializations that depend on a loaded config file
   loadConfig(getBookId()).then((result) => {
-    search.initialize();
+    search.initialize(searchInit(store));
 
     //no contents when result is false
     if (result) {
